de21fa797cb259cd2ae184aaf0934bd1b7b73c59
Krystian Potaczek <krystian.potaczek07@gmail.com>
2024-11-30 09:58:28 +0100

    Start

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int x, y;
+    x = 10;
+    y = 5; /* wiersz 7 */
+    y = x + y; /* wiersz 8 */
+    x = x*y; /* wiersz 9 */
+    printf("%d %d\n", x, y);
+    return 0;
+}
